e3f1de52312cb665ea8c76a97473f1debe82efc1
José Quiñónez <joserobertoq@outlook.com>
2024-06-01 08:11:15 -0600

    Add Weather Page

--- MauiWeather/obj/Debug/net8.0-ios/iossimulator-arm64/MauiWeather.AssemblyInfo.cs
+++ MauiWeather/obj/Debug/net8.0-ios/iossimulator-arm64/MauiWeather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiWeather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bd1cd2333db657d59e47829b035c0ad04c79c820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+b7a4b132dca7025ff2d0579ef4b80c8c47ee2b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiWeather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiWeather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
