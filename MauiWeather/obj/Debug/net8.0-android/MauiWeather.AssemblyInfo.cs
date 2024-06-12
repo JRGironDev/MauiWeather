9756cf956b454ba856b6304d0d5bb9bd63dd4c76
José Quiñónez <joserobertoq@outlook.com>
2024-06-12 14:34:18 -0600

    Add Cards

--- MauiWeather/obj/Debug/net8.0-android/MauiWeather.AssemblyInfo.cs
+++ MauiWeather/obj/Debug/net8.0-android/MauiWeather.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiWeather")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+faea516f84ef85cd923c886ef9ddeb8430fe64b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e179a53d11ae215e5c5f49db7c1592c8de05759c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiWeather")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiWeather")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
